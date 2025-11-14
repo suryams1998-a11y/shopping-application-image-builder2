cddb87aece2533a29362e6de46547087a6a77535
surya <surya@gmail.com>
2025-11-14 15:13:13 +0000

    terraform workflow script-infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,6 @@
-# keyPair Creation
-resource "aws_key_pair" "authentication_key" {
-  key_name   = "${var.project_name}-${var.project_environment}"
-  public_key = file("mykey.pub")
-  tags = {
-    "Name"        = "${var.project_name}-${var.project_environment}"
-  }
-}
+#security group
 
-# Creating  SecurityGroup For Webserver
 resource "aws_security_group" "webserver" {
-
   name        = "${var.project_name}-${var.project_environment}-webserver"
   description = "${var.project_name}-${var.project_environment}-webserver"
 
@@ -18,38 +9,7 @@ resource "aws_security_group" "webserver" {
   }
 }
 
-
-resource "aws_security_group_rule" "webserver_http" {
-  type              = "ingress"
-  security_group_id = aws_security_group.webserver.id
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-  
-}
-
-resource "aws_security_group_rule" "webserver_https" {
-  type              = "ingress"
-  security_group_id = aws_security_group.webserver.id
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-  
-}
-
-resource "aws_security_group_rule" "webserver_ssh" {
-  type              = "ingress"
-  security_group_id = aws_security_group.webserver.id
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-}
+#egress rule:
 
 resource "aws_security_group_rule" "webserver_egress" {
   type              = "egress"
@@ -59,36 +19,46 @@ resource "aws_security_group_rule" "webserver_egress" {
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   ipv6_cidr_blocks  = ["::/0"]
-  
+}
+
+#ingress rule
+
+resource "aws_security_group_rule" "webserver_ingress" {
+  for_each          = toset(var.webserver_ports)
+  type              = "ingress"
+  security_group_id = aws_security_group.webserver.id
+  from_port         = each.value
+  to_port           = each.value
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
 }
 
 
-# Creating Webserver Instance
+
+#webserver instance
 resource "aws_instance" "webserver" {
 
-  ami                    = var.ami_id
+  ami                    = data.aws_ami.application_image.image_id
   instance_type          = var.instance_type
   key_name               = "key-ohio"
-  vpc_security_group_ids = [ aws_security_group.webserver.id  ]
-  user_data              = file("setup.sh")
+  vpc_security_group_ids = [aws_security_group.webserver.id]
+
   tags = {
-    "Name"        = "webserver-${var.project_name}-${var.project_environment}"
+    Name = "webserver-${var.project_name}-${var.project_environment}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
-
-
 
 # Elastic IP
 resource "aws_eip" "webserver" {
   count = var.enable_public_ip ? 1 : 0
+
   tags = {
     Name = "webserver-${var.project_name}-${var.project_environment}-eip"
-  }
-
-
-  
-  lifecycle {
-    create_before_destroy = true
   }
 }
 
@@ -101,7 +71,7 @@ resource "aws_eip_association" "webserver" {
 
 # Route53 Record (EIP enabled)
 resource "aws_route53_record" "webserver_eip_enabled" {
-  count   = var.enable_public_ip == true ? 1 : 0
+  count   = var.enable_public_ip ? 1 : 0
   zone_id = data.aws_route53_zone.domain.zone_id
   name    = "${var.webserver_hostname}.${var.domain_name}"
   type    = "A"
@@ -109,9 +79,10 @@ resource "aws_route53_record" "webserver_eip_enabled" {
   records = [aws_eip.webserver[0].public_ip]
 }
 
+
 # Route53 Record (EIP disabled)
 resource "aws_route53_record" "webserver_eip_disabled" {
-  count   = var.enable_public_ip == false ? 1 : 0
+  count   = var.enable_public_ip ? 0 : 1
   zone_id = data.aws_route53_zone.domain.zone_id
   name    = "${var.webserver_hostname}.${var.domain_name}"
   type    = "A"
